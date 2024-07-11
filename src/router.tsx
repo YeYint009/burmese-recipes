cd179cfe2c2a326195e48c54be95a75043fea5b2
YeYint009 <blackskym96@gmail.com>
2024-07-11 19:05:01 +0900

    route change

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "./Layout";
-import HomePage from "./pages/homePage/HomePage";
 import MenuPage from "./pages/menuPage/MenuPage";
 
 export const router = createBrowserRouter([
